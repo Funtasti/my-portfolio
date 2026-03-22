98053c60c3070e36b309642321b7912d01d7b559
Sumit Kumar <sk9531823@gmail.com>
2026-03-22 22:30:23 +0530

    changes

--- src/pages/AboutDetail.tsx
+++ src/pages/AboutDetail.tsx
@@ -4,7 +4,7 @@ import { Code, Award, Calendar, type LucideIcon } from 'lucide-react' //Trophy,
 import { useScrollAnimation } from '../hooks/useScrollAnimation'
 import { smoothScrollTo } from '../utils/smoothScroll'
 import StatsCard from '../components/StatusCard'
-import resumePdf from '../assets/Sumit_kumar_naik_resume.pdf'
+import resumePdf from '../assets/SumitKumarNaik_Resume.pdf'
 
 interface Stat {
   icon: LucideIcon
